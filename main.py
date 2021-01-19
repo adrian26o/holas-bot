49d5f06f6ddd72f5ed1e33e4d18415e5c038ee8c
DaHolyCheese <kesitorayado@gmail.com>
2021-01-19 20:02:00 -0300

    Comando #say añadido.

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import discord
 from discord.ext import commands
 import os
 
-client = discord.Client(commands_prefix='#')
+client = commands.Bot(command_prefix=commands.when_mentioned_or("#"))
 token = os.environ.get('TOKEN')
 
 @client.event
 async def on_ready():
     print("HOLAS")
 
+@client.command()
+async def say(ctx, *, message):
+    if ctx.message.raw_mentions != "<[]>":
+        await ctx.send("No Menciones 😠")
+        return
+    await ctx.send(message)
+    await ctx.message.delete
+    
 client.run(token)
