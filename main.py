c3da733f42125206e850b41ae0459996163363f8
Leandro <leandro@pop-os.localdomain>
2021-02-06 00:16:45 -0300

    Bruh, comando de ping añadido y ajustes en comando de say.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import os
-
+import json
 client = commands.Bot(command_prefix=commands.when_mentioned_or("#"))
 token = os.environ.get('TOKEN')
 
@@ -10,12 +10,21 @@ async def on_ready():
     print("HOLAS")
 
 @client.command()
+async def ping(ctx):
+    await ctx.send(f"{round(client.latency*1000)}ms")
+
+@client.command()
 async def say(ctx, *, message):
-    if len(ctx.message.raw_mentions) != 0:
-        print(ctx.message.raw_mentions)
+    mentions = ctx.message.raw_mentions
+    if len(mentions) != 0:
+        print(mentions)
         await ctx.send("No Menciones 😠")
+        mentions.clear()
+        return
+    if ("@everyone") or ("@here") in message:
+        await ctx.send("No Hagas Everyone 😠")
         return
     await ctx.send(message)
     await ctx.message.delete
-    
+
 client.run(token)
