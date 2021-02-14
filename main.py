59cf14a4c7041eb34b0438156ec7de07a296b58f
adrian26o <75555468+adrian26o@users.noreply.github.com>
2021-02-14 17:22:10 -0600

    veamos si ahora se puede...

--- main.py
+++ main.py
@@ -296,9 +296,9 @@ async def hola(ctx, *, member: discord.Member = None):
 
 #Google, Realiza una busqueda usando una api de google y devuelve los 5 primeros resultados
 @client.command()
-async def google(ctx,*,search = None):
+async def google(ctx, * ,search = None):
     if search == None:
-        embed = discord.Embed(title="Eh...", description=f"{ctx.message.author.mention} Haz una búsqueda.", colour= discord.Colour(0xf5a623))
+        embed = discord.Embed(title="Error", description=f"{ctx.message.author.mention} Haz una búsqueda.", colour= discord.Colour(0xf5a623))
         await ctx.channel.send(embed=embed)
     else:
         search2 = search(search,stop=5,lang="es")
