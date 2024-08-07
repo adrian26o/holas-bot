8bfbbefafaed0b979cc713340d341e90d896210b
Zhen <Zhennyak@hotmail.com>
2024-08-06 20:51:06 -0600

    WAAA config.json

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,8 +1,8 @@
 import {REST, Routes} from "discord.js"
-import * as config from "./config.json"
+// import * as config from "./config.json"
 import { commands } from "./commands/init";
 
-const rest = new REST().setToken(config.token);
+const rest = new REST().setToken(process.env.BOT_TOKEN || "");
 
 
 /*
@@ -18,15 +18,16 @@ commands.forEach(v => {
 */
 
 const commandDataList = commands.map(v => v.data.toJSON());
-
+const guildID = "796962500373250069" // Server ID of tests
+const userID = "796797535208341544" // Bot  ID
 
 const args = process.argv.slice(2);
 if(args.some(v => v === "--test")) {
-	rest.put(Routes.applicationGuildCommands(config.user.id, config.testGuild.id), {
+	rest.put(Routes.applicationGuildCommands("", ""), {
 		body: commandDataList
 	});
 } else {
-	rest.put(Routes.applicationCommands(config.user.id), {
+	rest.put(Routes.applicationCommands(""), {
 		body: commandDataList
 	})
 }
